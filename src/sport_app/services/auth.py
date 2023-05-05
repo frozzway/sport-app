270cdd571f37d6e86903d4fe79110704d3806cde
FrozzWay <frozz.way@gmail.com>
2023-05-05 10:21:53 +0500

    05.05

--- src/sport_app/services/auth.py
+++ src/sport_app/services/auth.py
@@ -24,7 +24,7 @@ def get_current_staff(token: str = Depends(oauth2_scheme)) -> models.Staff:
 
 
 def validate_admin_access(staff_member: models.Staff = Depends(get_current_staff)) -> models.Staff:
-    if staff_member.role != "staff_role.admin":
+    if staff_member.role != str(tables.Roles.admin):
         raise HTTPException(status.HTTP_403_FORBIDDEN)
     return staff_member
 
@@ -35,7 +35,7 @@ def validate_operator_access(staff_member: models.Staff = Depends(get_current_st
         return staff_member
     except HTTPException:
         pass
-    if staff_member.role != "staff_role.operator":
+    if staff_member.role != str(tables.Roles.operator):
         raise HTTPException(status.HTTP_403_FORBIDDEN)
     return staff_member
 
